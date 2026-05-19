9b1c897880d4325a5ca4708076d3e96a180753b0
aybarceferino-create <aybarceferino@gmail.com>
2026-05-19 14:18:57 -0300

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,13 @@
+
+'use client'
+export default function Page(){
+return (
+<div style={{padding:'40px',fontFamily:'Arial'}}>
+<h1>CEFE</h1>
+<p>Jurisprudencia Argentina IA</p>
+<input placeholder="Buscar jurisprudencia..." style={{padding:'10px',width:'300px'}} />
+<br/><br/>
+<button>Buscar</button>
+<p>Proyecto base listo para continuar.</p>
+</div>
+)}
